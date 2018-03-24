3b74c4173c5accfa79cd1d318c8df1297f789816
richardjohns <richard.johns193@gmail.com>
2018-03-24 21:06:57 +1300

    changes to movies controller

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -10,6 +10,12 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate", // name of route
+                "movies/released/{year}/{month}", // url pattern
+                new {controller = "Movies", action = "ByReleaseDate"} // anonymous object used for default
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
