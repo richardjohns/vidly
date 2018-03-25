e9d96fbb94dc3c1fac0fde80dfe053a318e13e47
richardjohns <richard.johns193@gmail.com>
2018-03-25 22:33:50 +1300

    pages work with bootstrap navbar partial

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,18 +12,18 @@ namespace Vidly
             routes.MapMvcAttributeRoutes(); // method enables attribute routing rather than convention-based routing
 
             //Convention-based routing
-            //routes.MapRoute(
-            //    "MoviesByReleaseDate", // name of route
-            //    "movies/released/{year}/{month}", // url pattern
-            //    new {controller = "Movies", action = "ByReleaseDate"}, // anonymous object used for default
-            //    new {year = @"\d{4}", month = @"\d{2}" // anon object for setting constraints on url params
-            //);
+            routes.MapRoute(
+                "MoviesByReleaseDate", // name of route
+                "movies/released/{year}/{month}", // url pattern
+                new {controller = "Movies", action = "ByReleaseDate"}, // anonymous object used for default
+                new { year = @"\d{4}", month = @"\d{2}" } // anon object for setting constraints on url params
+            );
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ Vidly/Views/Shared/_NavBar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Vidly.Views.Shared
+{
+    public class _NavBar
+    {
+        public _NavBar()
+        {
+        }
+    }
+}
